0aab905612d14919804dbf7d6d029c3df5d77444
kpacha <kpacha666@gmail.com>
2018-02-17 23:31:11 +0100

    405 template removed

--- engine/templates.go
+++ engine/templates.go
@@ -14,19 +14,6 @@ var (
 	<p>You might want to customize this file by editing <code>static/404</code></p>
 </body>`
 
-	default405Tmpl = `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<title>Method not supported</title>
-</head>
-<body class="text-center">
-	<h1 class="my-5">Method not allowed!</h1>
-	<p>The requested HTTP method is not supported</p>
-	<p>You might want to customize this file by editing <code>static/405</code></p>
-</body>`
-
 	default500Tmpl = `<!DOCTYPE html>
 <html lang="es">
 <head>
